16a527dd2ac061ae4a7402dae8f76b800c72b283
gonpineiro <gon.pineiro@gmail.com>
2022-02-14 10:59:20 -0300

    Identacion del codigo

--- api/v1/usuario/index.php
+++ api/v1/usuario/index.php
@@ -6,58 +6,61 @@ use App\Controllers\UsuarioController;
 
 $token = getBearerToken();
 
-if ($token == USUARIO_KEY) {
-	$usuarioController = new UsuarioController();
+$usuarioController = new UsuarioController();
 
-	if ($rm == 'GET') {
-		if (isset($_GET) && count($_GET) > 0) {
-			$usuario = $usuarioController->get($_GET);
-			if (!$usuario instanceof ErrorException) {
-				if ($usuario) {
-					sendRes($usuario);
-					header("HTTP/1.1 200 OK");
-				} else {
-					sendRes(null, 'No se encontro el usuario', $_GET);
-					header("HTTP/1.1 403 Not Found");
-				}
+/* Metodo GET */
+if ($token == USUARIO_KEY && $rm == 'GET') {
+	if (isset($_GET) && count($_GET) > 0) {
+		$usuario = $usuarioController->get($_GET);
+		if (!$usuario instanceof ErrorException) {
+			if ($usuario) {
+				sendRes($usuario);
+				header("HTTP/1.1 200 OK");
 			} else {
-				sendRes(null, $usuario->getMessage(), $_GET);
+				sendRes(null, 'No se encontro el usuario', $_GET);
 				header("HTTP/1.1 403 Not Found");
-			};
+			}
 		} else {
-			$usuarios = $usuarioController->index(['TOP' => 10]);
-			if (!$usuarios instanceof ErrorException) {
-				sendRes($usuarios);
-				header("HTTP/1.1 201 OK");
-			} else {
-				sendRes(null, $usuarios->getMessage(), $_GET);
-				header("HTTP/1.1 400 Error");
-			};
-		}
+			sendRes(null, $usuario->getMessage(), $_GET);
+			header("HTTP/1.1 403 Not Found");
+		};
+	} else {
+		$usuarios = $usuarioController->index(['TOP' => 10]);
+		if (!$usuarios instanceof ErrorException) {
+			sendRes($usuarios);
+			header("HTTP/1.1 201 OK");
+		} else {
+			sendRes(null, $usuarios->getMessage(), $_GET);
+			header("HTTP/1.1 400 Error");
+		};
 	}
 
-	// Crear un nuevo post
-	if ($rm == 'POST') {
-		$usuario = $usuarioController->store($_POST);
-		sendRes(['ReferenciaID' => $usuario]);
-		header("HTTP/1.1 200 OK");
-		exit();
-	}
+	exit();
+}
 
-	//Borrar
-	if ($rm == 'DELETE') {
-		header("HTTP/1.1 200 OK");
-		exit();
-	}
+/* Metodo POST */
+if ($token == USUARIO_KEY &&  $rm == 'POST') {
+	$usuario = $usuarioController->store($_POST);
+	sendRes(['ReferenciaID' => $usuario]);
+	header("HTTP/1.1 200 OK");
+	exit();
+}
 
-	//Actualizar
-	if ($rm == 'PUT') {
-		header("HTTP/1.1 200 OK");
-		exit();
-	}
+/* Metodo DELETE */
+if ($token == USUARIO_KEY &&  $rm == 'DELETE') {
+	header("HTTP/1.1 200 OK");
+	exit();
+}
 
-	header("HTTP/1.1 400 Bad Request");
+/* Metodo PUT */
+if ($token == USUARIO_KEY && $rm == 'PUT') {
+	header("HTTP/1.1 200 OK");
+	exit();
+}
+
+if ($token != USUARIO_KEY) {
+	header("HTTP/1.1 401 Unauthorized");
 } else {
-	sendRes(null, 'Token de seguridad erroneo', null);
+	header("HTTP/1.1 400 Bad Request");
 }
 exit();
