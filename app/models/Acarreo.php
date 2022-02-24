c250b53cce50ec2e0c54d70f1231658f9d1b6e2c
gonpineiro <gon.pineiro@gmail.com>
2022-02-24 10:28:56 -0300

    Acarreo

--- app/models/Acarreo.php
+++ app/models/Acarreo.php
@@ -15,11 +15,12 @@ class Acarreo
                 a.PATENTE AS patente,
                 m.NOMBRE AS motivo,
                 p.NOMBRE AS playa,
-                p.DESCRIPCION AS direccion
+                p.DESCRIPCION AS direccion,
+                a.FECHA_HORA as fecha
             FROM dbo.wapUsuarios wu
                 LEFT JOIN AC_ACARREO a ON a.ID_PERSONA = wu.PersonaID 
                 LEFT JOIN AC_MOTIVO m ON m.ID_MOTIVO = a.ID_MOTIVO
-                LEFT JOIN AC_PLAYAs p ON p.ID_PLAYA= a.ID_PLAYA
+                LEFT JOIN AC_PLAYA p ON p.ID_PLAYA= a.ID_PLAYA
             WHERE wu.ReferenciaID = $id and a.BORRADO_LOGICO = 'NO'";
 
         try {
