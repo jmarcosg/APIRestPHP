c398d3f27c6c037867c22bec7cb2a9e6bae80d0c
gonpineiro <gon.pineiro@gmail.com>
2022-03-07 09:13:58 -0300

    Comparacion de fechas en licencia

--- app/models/LicenciaConducir.php
+++ app/models/LicenciaConducir.php
@@ -23,9 +23,23 @@ class LicenciaConducir
             $conn = new BaseDatos();
             $query =  $conn->query($sql);
             $result = $conn->fetch_assoc($query);
+            $result = $this->changeResultFormat($result);
             return $result;
         } catch (\Throwable $th) {
             return $th;
         }
+    }
+
+    private function changeResultFormat(array $result)
+    {
+        $result['estado'] = 'Vigente';
+        if ($result['venc']) {
+            $result['venc'] = substr($result['venc'], 0, 10);
+            $arrayFechas = compararFechas($result['venc'], 'days', 'Y-m-d');
+            if ($arrayFechas['dif'] <= 7) $result['estado'] = 'Por vencer';
+            if ($arrayFechas['date'] <= $arrayFechas['now']) $result['estado'] =  'Vencida';
+        }
+
+        return $result;
     }
 }
