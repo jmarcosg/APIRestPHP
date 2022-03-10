e6d80258e09b4fd2add21241057e6ba0a87d2c60
gonpineiro <gon.pineiro@gmail.com>
2022-03-10 09:23:59 -0300

    Configuracion de renaper

--- api/v1/renaper/index.php
+++ api/v1/renaper/index.php
@@ -7,7 +7,20 @@ $renaperController = new RenaperController();
 /* Metodo GET */
 if ($token == RENAPER_KEY && $url['method'] == 'GET') {
 	if (isset($_GET) && isset($_GET['sexo']) && isset($_GET['dni'])) {
-		$renaper = $renaperController->getData($_GET['sexo'], $_GET['dni']);
+
+		if (!isset($_GET['type'])) $_GET['type'] = '';
+		switch ($_GET['type']) {
+			case 'imagen';
+				$renaper = $renaperController->getImage($_GET['sexo'], $_GET['dni']);
+				break;
+			case 'person';
+				$renaper = $renaperController->getPersonData($_GET['sexo'], $_GET['dni']);
+				break;
+			default:
+				$renaper = $renaperController->getData($_GET['sexo'], $_GET['dni']);
+				break;
+		}
+
 		if (!$renaper instanceof ErrorException) {
 			sendRes($renaper);
 		} else {
--- app/controllers/RenaperController.php
+++ app/controllers/RenaperController.php
@@ -11,4 +11,36 @@ class RenaperController
         $renaper = new Renaper();
         return $renaper->getData($gender, $dni);
     }
+
+    public function getImage($gender, $dni)
+    {
+        $renaper = new Renaper();
+        $renaper = $renaper->getData($gender, $dni);
+
+        return [
+            'imagen' => $renaper->imagen,
+            'urlImagen' => $renaper->urlImagen
+        ];
+    }
+
+    public function getPersonData($gender, $dni)
+    {
+        $renaper = new Renaper();
+        $renaper = $renaper->getData($gender, $dni);
+
+        return [
+            'referenciaID' => $renaper->referenciaID,
+            'documento' => $renaper->documento,
+            'cuil' => $renaper->cuil,
+            'razonSocial' => $renaper->razonSocial,
+            'fechaDeNacimiento' => $renaper->fechaDeNacimiento,
+            'edad' => $renaper->edad,
+            'genero' => [
+                'textID' => $renaper->genero->textID,
+                'value' => $renaper->genero->value,
+            ],
+            'celular' => $renaper->celular,
+            'correoElectronico' => $renaper->correoElectronico,
+        ];
+    }
 }
