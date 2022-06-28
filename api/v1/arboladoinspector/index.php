b77fe2297ef9c6d08604b407f8c357ccab7db9aa
gonpineiro <gon.pineiro@gmail.com>
2022-06-28 08:48:56 -0300

    Modulo inspector completado

--- api/v1/arboladoinspector/index.php
+++ api/v1/arboladoinspector/index.php
@@ -1,71 +1,41 @@
 <?php
 
 use App\Controllers\Arbolado\Arb_InspectorController;
-use App\Controllers\Arbolado\Arb_PodadorController;
 
 $arbInspectorController = new Arb_InspectorController();
-$arbPodadorController = new Arb_PodadorController();
 
 /* Metodo GET */
 if ($url['method'] == 'GET') {
-	if (isset($_GET) && count($_GET) > 0 && isset($_GET['action'])) {
-		$action = $_GET['action'];
-		unset($_GET['action']);
+	$action = $_GET['action'];
+	unset($_GET['action']);
 
-		switch ($action) {
-			case '0':
-				/* Mostramos todos los inspectores */
-				Arb_InspectorController::index();
-				break;
+	switch ($action) {
+		case '0':
+			/* Mostramos todos los inspectores */
+			Arb_InspectorController::index();
+			break;
 
-			case '1':
-				/* Obtenemos una solicitud puntual */
-				$inspector = $arbInspectorController->get(['id' => $_GET['id']]);
-				break;
+		case '1':
+			/* Obtenemos una solicitud puntual */
+			Arb_InspectorController::get(['id' => $_GET['id']]);
+			break;
 
-			default:
-				$inspector = new ErrorException('El action no es valido');
-				break;
-		}
-
-
-		if (!$inspector instanceof ErrorException) {
-			if ($inspector !== false) {
-				sendRes($inspector);
-			} else {
-				sendRes(null, 'No se encontro la evaluación', $_GET);
-			}
-		} else {
-			sendRes(null, $arbolado->getMessage(), $_GET);
-		};
+		default:
+			$error = new ErrorException('El action no es valido');
+			sendRes(null, $error->getMessage(), $_GET);
+			exit;
+			break;
 	}
-	eClean();
 }
 
 /* Metodo POST */
-if ($url['method'] == 'POST') {
-	Arb_InspectorController::store($_POST);
-}
+if ($url['method'] == 'POST') Arb_InspectorController::store();
 
 /* Metodo PUT */
-if ($url['method'] == 'PUT') {
-	parse_str(file_get_contents('php://input'), $_PUT);
-	$id = $url['id'];
-	$arbolado = $arbPodadorController->update($_PUT, $id);
-
-	if (!$arbolado instanceof ErrorException) {
-		$_PUT['id'] = $id;
-		sendRes($_PUT);
-	} else {
-		sendRes(null, $arbolado->getMessage(), ['id' => $id]);
-	};
-	eClean();
-}
+if ($url['method'] == 'PUT') Arb_InspectorController::update($_PUT, $url['id']);
 
 /* Metodo DELETE */
-if ($url['method'] == 'DELETE') {
-	Arb_InspectorController::delete($url['id']);
-}
+if ($url['method'] == 'DELETE') Arb_InspectorController::delete($url['id']);
 
 header("HTTP/1.1 200 Bad Request");
 
--- app/controllers/Arbolado/Arb_InspectorController.php
+++ app/controllers/Arbolado/Arb_InspectorController.php
@@ -34,25 +34,35 @@ class Arb_InspectorController
     {
         $data = new Arb_Inspector();
         $data = $data->get($params)->value;
-        return $data;
+
+        if (!$data instanceof ErrorException) {
+            if ($data !== false) {
+                sendRes($data);
+            } else {
+                sendRes(null, 'No se encontro la evaluación', $_GET);
+            }
+        } else {
+            sendRes(null, $data->getMessage(), $_GET);
+        };
+        exit;
     }
 
-    public static function store($res)
+    public static function store()
     {
-        $exist = self::get(['dni' => $res['dni']]);
+        $data = new Arb_Inspector();
+        $exist = $data->get(['dni' => $_POST['dni']])->value;
 
         if (!$exist) {
-            /* Guardamos el inspector */
-            $data = new Arb_Inspector();
-            $data->set($res);
+            /* Guardamos el data */
+            $data->set($_POST);
             $id = $data->save();
 
             if (!$id instanceof ErrorException) {
                 /* Generamos registro para la auditoria */
                 $audit = new Arb_Audit();
                 $audit->set([
-                    'id_usuario' => $res['id_usuario_admin'],
-                    'id_wappersonas' => $res['id_wappersonas_admin'],
+                    'id_usuario' => $_POST['id_usuario_admin'],
+                    'id_wappersonas' => $_POST['id_wappersonas_admin'],
                     'id_inspector' => $id,
                     'accion' => 'agrego',
                 ]);
@@ -63,15 +73,24 @@ class Arb_InspectorController
                 sendRes(null, $id->getMessage(), $_GET);
             };
         } else {
-            sendRes(null, 'Ya se encuentra registrado', ['dni' => $res['dni']]);
+            sendRes(null, 'Ya se encuentra registrado', ['dni' => $_POST['dni']]);
         }
         exit;
     }
 
-    public function update($req, $id)
+    public static function update($id)
     {
+        parse_str(file_get_contents('php://input'), $_PUT);
         $data = new Arb_Inspector();
-        return $data->update($req, $id);
+        $data = $data->update($_PUT, $id);
+
+        if (!$data instanceof ErrorException) {
+            $_PUT['id'] = $id;
+            sendRes($_PUT);
+        } else {
+            sendRes(null, $data->getMessage(), ['id' => $id]);
+        };
+        exit;
     }
 
     public static function delete($id)
