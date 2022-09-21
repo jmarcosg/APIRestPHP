fc1a10339fe16df104c4b184b436f9b9412fe8a1
gonpineiro <gon.pineiro@gmail.com>
2022-09-21 14:35:57 -0300

    Deploy licencia

--- app/models/Weblogin/Weblogin.php
+++ app/models/Weblogin/Weblogin.php
@@ -3,11 +3,15 @@
 namespace App\Models\Weblogin;
 
 use App\Models\BaseModel;
+use ErrorException;
 
 class Weblogin extends BaseModel
 {
     public static function saveLog($error, $class, $function)
     {
+        if (!$error instanceof ErrorException) {
+            $error = new ErrorException($error);
+        }
         logFileEE('v1/weblogin', $error, $class, $function);
     }
 }
