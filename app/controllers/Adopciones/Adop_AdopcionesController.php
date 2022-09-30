9c10d70b5f064c580da6ff0c968ad7b20a540510
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2022-09-29 21:15:14 -0300

    ✨ Controladores adopciones

--- app/controllers/Adopciones/Adop_AdopcionesController.php
+++ app/controllers/Adopciones/Adop_AdopcionesController.php
@@ -3,25 +3,25 @@
 namespace App\Controllers\Adopciones;
 
 use App\Connections\BaseDatos;
-use App\Models\Adopciones\Adop_Animal;
+use App\Models\Adopciones\Adop_Adopcion;
 
-class Adop_AnimalesController
+class Adop_AdopcionesController
 {
     public function __construct()
     {
-        $GLOBALS['exect'][] = 'ADOP_Animales';
+        $GLOBALS['exect'][] = 'ADOP_Adopciones';
     }
 
     public static function index($param = [], $ops = [])
     {
-        $data = new Adop_Animal();
+        $data = new Adop_Adopcion();
         $data = $data->list($param, $ops)->value;
         return $data;
     }
 
     public static function store($res)
     {
-        $data = new Adop_Animal();
+        $data = new Adop_Adopcion();
         $data->set($res);
         return $data->save();
     }
--- app/controllers/Adopciones/Adop_VecinosController.php
+++ app/controllers/Adopciones/Adop_VecinosController.php
@@ -3,25 +3,25 @@
 namespace App\Controllers\Adopciones;
 
 use App\Connections\BaseDatos;
-use App\Models\Adopciones\Adop_Animal;
+use App\Models\Adopciones\Adop_Vecino;
 
-class Adop_AnimalesController
+class Adop_VecinosController
 {
     public function __construct()
     {
-        $GLOBALS['exect'][] = 'ADOP_Animales';
+        $GLOBALS['exect'][] = 'ADOP_Vecinos';
     }
 
     public static function index($param = [], $ops = [])
     {
-        $data = new Adop_Animal();
+        $data = new Adop_Vecino();
         $data = $data->list($param, $ops)->value;
         return $data;
     }
 
     public static function store($res)
     {
-        $data = new Adop_Animal();
+        $data = new Adop_Vecino();
         $data->set($res);
         return $data->save();
     }
