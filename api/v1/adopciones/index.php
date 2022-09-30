1ec1c91908ec67d67a6ecaa4358077646c0ca331
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2022-09-29 21:14:47 -0300

    🚧 Iniciando casos get y post

--- api/v1/adopciones/index.php
+++ api/v1/adopciones/index.php
@@ -1,71 +1,99 @@
 <?php
 
-use App\Controllers\Adopciones\Adop_AnimalController;
-use App\Controllers\LicenciaComercial\Lc_SolicitudController;
-use App\Controllers\LicenciaComercial\Lc_SolicitudHistorialController;
-use App\Controllers\LicenciaComercial\Lc_DocumentoController;
-use App\Controllers\LicenciaComercial\Lc_RubroController;
-use App\Controllers\Common\TipoDocumentoController;
+use App\Controllers\Adopciones\Adop_AnimalesController;
+use App\Controllers\Adopciones\Adop_VecinosController;
+use App\Controllers\Adopciones\Adop_AdopcionesController;
 
-/* Metodo GET */
+use App\Traits\QRIdentificacion\RequestGenerarQR;
+use App\Traits\QRIdentificacion\RequestGenerarVCard;
 
-if ($url['method'] == 'GET') {
-	$action = $_GET['action'];
-	unset($_GET['action']);
+if ($url['method'] == "GET") {
+	if (isset($_GET['action'])) {
 
-	switch ($action) {
+		switch ($_GET['action']) {
+			case '1':
+				// Listado de animales
+				$animalesController = new Adop_AnimalesController();
+				$data = $animalesController->index();
 
-		case '1':
-			echo "hola";
-			exit;
+				if (count($data) == 0) {
+					$data = [
+						'qr' => null,
+						'error' => "QR no encontrado"
+					];
+				} else {
+					$data['error'] = null;
+				}
+				break;
+			case '2':
+				// Listado de vecinos
+				$data = Adop_VecinosController::index();
+
+				echo var_dump($data);
+
+				if (count($data) == 0) {
+					$data = [
+						'usuario' => null,
+						'error' => "Vecino no encontrado"
+					];
+				} else {
+					$data['error'] = null;
+				}
+				break;
+
+			case '3':
+				// Listado de adopciones
+				$data = Adop_AdopcionesController::index();
+				break;
+		}
+	} else {
+		$data = [
+			'adopcion' => null,
+			'error' => "Adopcion no encontrada"
+		];
 	}
+
+	echo json_encode($data);
 }
 
-/* Metodo POST */
-if ($url['method'] == 'POST') {
+if ($url['method'] == "POST") {
 	switch ($_POST['action']) {
 		case '1':
-			echo "hola";
-			exit;
+			// Crear animal
+			Adop_AnimalesController::store($_POST);
+			$data = [
+				'nombre' => $_POST['nombre'],
+				'edad' => $_POST['raza'],
+				'tamanio' => $_POST['tamanio'],
+				'castrado' => $_POST['castrado'],
+				'descripcion' => $_POST['descripcion']
+			];
+			$persona = QRI_PersonaController::index($data)[0];
+			$usuario = QRI_UsuarioController::index(['email' => $_POST['mailUsuario']])[0];
+			$qrs = QRI_CodigoQRController::index();
+
+			$dataQR = [
+				'id_usuario' => $usuario['id'],
+				'id_persona_identificada' => $persona['id'],
+				'qr_path' => 'E:/Dataserver/Replica/projects_files/qr-identificacion/' . (count($qrs) + 1) . "/",
+				'qr_token' => md5($persona['email'] . $usuario['email'] . (count($qrs) + 1))
+			];
+
+			if (QRI_CodigoQRController::store($dataQR)) {
+				$dataQR['sessionkey'] = $_POST['sessionkey'];
+				$dataQR['id_solicitud'] = count($qrs) + 1;
+				$resp = RequestGenerarQR::sendRequest($dataQR);
+
+				echo json_encode($resp);
+			}
+			break;
+
+		case '2':
+			QRI_UsuarioController::store($_POST);
+			break;
+
+		case '3':
+			echo json_encode(RequestGenerarVCard::generateVcard($_POST));
+			break;
 	}
 }
-
-/* Metodo PUT */
-if ($url['method'] == 'PUT') {
-	parse_str(file_get_contents('php://input'), $_PUT);
-	$id = $url['id'];
-
-	$action = $_PUT['action'];
-	unset($_PUT['action']);
-
-	switch ($action) {
-		case '1':
-			echo "hola";
-			exit;
-	}
-
-
-	if (!$lc instanceof ErrorException) {
-		$_PUT['id'] = $id;
-		sendRes($_PUT);
-	} else {
-		sendRes(null, $adopciones->getMessage(), ['id' => $id]);
-	};
-	eClean();
-}
-
-/* Metodo DELETE */
-if ($url['method'] == 'DELETE') {
-	$id = $url['id'];
-	$adopciones = $adopSolicitudController->delete($url['id']);
-	if (!$adopciones instanceof ErrorException) {
-		sendRes(['ReferenciaID' => $id]);
-	} else {
-		sendRes(null, $adopciones->getMessage(), ['ReferenciaID' => $id]);
-	};
-	eClean();
-}
-
-header("HTTP/1.1 200 Bad Request");
-
-eClean();
