a772c626ead52c4715cfb97df974206e1d44adb5
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2022-09-30 13:37:15 -0300

    ✨ Fillable animal

--- app/models/Adopciones/Adop_Animal.php
+++ app/models/Adopciones/Adop_Animal.php
@@ -4,20 +4,32 @@ namespace App\Models\Adopciones;
 
 use App\Models\BaseModel;
 
-class Adop_Vecino extends BaseModel
+class Adop_Animal extends BaseModel
 {
-    protected $table = 'ADOP_vecinos';
+    protected $table = 'adop_animales';
     protected $logPath = 'v1/adopciones';
     protected $identity = 'id';
 
     protected $fillable = [
+        'imagen1_path',
+        'imagen2_path',
         'nombre',
-        'dni',
-        'email',
-        'email_alternativo',
-        'telefono',
-        'telefono_alternativo',
-        'ciudad',
-        'domicilio'
+        'edad',
+        'raza',
+        'tamanio',
+        'castrado',
+        'descripcion',
+        'adoptado',
+        'fecha_ingreso',
+        'fecha_modificacion',
     ];
+
+    public $filesUrl = FILE_PATH . 'adopciones/';
+
+    // public function saveAnimal($idSolicitud, $solicitud)
+    // {
+    //     $params = ['id_solicitud' => $idSolicitud, 'id_tipo_documento' => 1, 'verificado' => 0];
+    //     $this->set($params);
+    //     $this->save();
+    // }
 }
