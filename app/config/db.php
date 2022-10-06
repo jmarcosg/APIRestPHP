e76592a2e4adb58248ecfec28fdcf82505d6ce22
gonpineiro <gon.pineiro@gmail.com>
2022-10-06 13:48:01 -0300

    Cambio DB para replica

--- app/config/db.php
+++ app/config/db.php
@@ -4,6 +4,6 @@
 define('DB_HOST', PROD ? $_ENV['DB_HOST'] : '128.53.15.3');
 define('DB_USER',  PROD ? $_ENV['DB_USER'] : 'userturnos');
 define('DB_PASS',  PROD ? $_ENV['DB_PASS'] : 'turnero16');
-define('DB_NAME',  PROD ? $_ENV['DB_NAME'] : 'info');
+define('DB_NAME',  PROD ? $_ENV['DB_NAME'] : 'infoprueba');
 define('DB_PORT',  PROD ? $_ENV['DB_PORT'] : '3306');
 define('DB_CHARSET',  PROD ? $_ENV['DB_CHARSET'] : 'utf8');
