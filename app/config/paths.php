866d6ecfca1e87761a88180da5d82a93b39f892c
gonpineiro <gon.pineiro@gmail.com>
2022-11-04 20:47:06 -0300

    Limpieza en paths.php

--- app/config/paths.php
+++ app/config/paths.php
@@ -1,19 +1,17 @@
 <?php
-
+/* Raiz del proyecto */
 define('ROOT_PATH', substr(__DIR__, 0, strlen(__DIR__) - 10));
 define('PUBLIC_PATH', ROOT_PATH . 'public');
+
 /* define('FILE_PATH_LOCAL', ROOT_PATH . 'files/'); */
 define('V1_PATH', ROOT_PATH . 'api/v1/');
-
-/* Necesitamos definir correctamente esta ruta */
-define('FILE_PATH_PROD', ROOT_PATH . 'files/');
 
 define('VIEW_PATH', ROOT_PATH . 'public/views');
 define('APP_PATH', ROOT_PATH . 'app');
 define('CON_PATH', ROOT_PATH . 'app/connections');
 define('UTIL_PATH', ROOT_PATH . 'app/utils');
 define('LOG_PATH', ROOT_PATH . 'logs/');
-define('TEM_PATH', ROOT_PATH . 'temp/');
+define('TEMP_PATH', ROOT_PATH . 'temp/');
 
 /* Path donde se guardan los archivos para poder hacer envio de email adjuntando dichos archivos */
 define('ADJUNTOS_PATH', "E:\Dataserver\Produccion\Adjuntos\\");
