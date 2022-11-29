6131ba960cba0503224c498ed141cee2ae511d16
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2022-11-28 21:13:56 -0300

    ✨ dotnev para prod

--- public/views/adopciones/config.php
+++ public/views/adopciones/config.php
@@ -8,6 +8,9 @@ require ROOT_PATH . 'app/utils/funciones.php';
 $dotenv = \Dotenv\Dotenv::createImmutable(ROOT_PATH);
 $dotenv->load();
 
+$dotenv = \Dotenv\Dotenv::createImmutable("./");
+$dotenv->load();
+
 define('DB_HOST', $_ENV['DB_HOST']);
 define('DB_USER', $_ENV['DB_USER']);
 define('DB_PASS', $_ENV['DB_PASS']);
