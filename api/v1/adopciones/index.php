5ba70d3b4fc4233d88a999e27bf9805ba23d749a
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2022-12-13 12:07:00 -0300

    test

--- api/v1/adopciones/index.php
+++ api/v1/adopciones/index.php
@@ -241,7 +241,7 @@ if ($url['method'] == "GET") {
 if ($url['method'] == "POST") {
 	switch ($_POST['action']) {
 		case 'an1':
-			//* Cargar animal
+			//* Cargar animall
 			$date = new DateTime('now');
 			$date->setTimezone(new DateTimeZone('America/Argentina/Buenos_Aires'));
 			$currentTime = $date->format('Y-m-d H:i:s');
