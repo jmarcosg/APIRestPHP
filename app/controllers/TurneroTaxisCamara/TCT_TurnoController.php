c1ebdd81ca6b704b13316c462243751e9f5dd4d6
santiagoScantamburlo <santiago.scantamburlo@est.fi.uncoma.edu.ar>
2023-05-15 12:34:57 -0300

    arreglito

--- app/controllers/TurneroTaxisCamara/TCT_TurnoController.php
+++ app/controllers/TurneroTaxisCamara/TCT_TurnoController.php
@@ -89,7 +89,7 @@ class TCT_TurnoController
         if (count($turnosPersona) > 0) {
             $fechaObj = new TCT_Fecha();
             $fechaTurno = $fechaObj->list(["id" => $turnosPersona[0]['fecha_id']])->value;
-            $turnosPersona['fecha'] = $fechaTurno[0];
+            $turnosPersona[0]['fecha'] = $fechaTurno[0];
             unset($turnosPersona[0]['fecha_id']);
             return ["success" => $turnosPersona[0], "error" => null];
         }
